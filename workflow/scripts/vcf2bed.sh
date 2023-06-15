d3a0a109e34ad919d38d4658e508b3390d343297
rpensch <rar.pensch@gmail.com>
2023-06-15 11:44:19 +0200

    fix sim file sorting

--- workflow/scripts/vcf2bed.sh
+++ workflow/scripts/vcf2bed.sh
@@ -14,19 +14,22 @@ convert2bed -i vcf --snvs | awk -v OFS='\t' '{print $1,$2,$3,$(NF)}' | sort -k1,
 # Convert sim vcfs to bed files
 zcat $sim | awk -v OFS='\t' -v sample="$sample" '{print $0,sample":"$1":"$2":"$4":"$5}' |
 convert2bed -i vcf --insertions | awk -v OFS='\t' '{print $1,$2,$3,$(NF)}' | sort -k1,1 -k2,2n \
->> $sim_out
+>> $sim_out.tmp
 zcat $sim | awk -v OFS='\t' -v sample="$sample" '{print $0,sample":"$1":"$2":"$4":"$5}' |
 convert2bed -i vcf --deletions |  awk -v OFS='\t' '{print $1,$2,$3,$(NF)}' | sort -k1,1 -k2,2n \
->> $sim_out
+>> $sim_out.tmp
 # Handle MNPs manually
 zgrep -v '^#' $sim | 
 awk -v OFS='\t' -v sample="$sample" '{ if (length($4)==length($5)) { print $0,sample":"$1":"$2":"$4":"$5} }' |
 while read chrom pos id ref alt qual filter info format tumor normal mutation_id ; do
     
     len=`echo $ref | awk '{print length($1)}'`
-    printf "$chrom\t$(($pos-1))\t$(($pos-1+$len))\t$mutation_id\n" >> $sim_out
+    printf "$chrom\t$(($pos-1))\t$(($pos-1+$len))\t$mutation_id\n" >> $sim_out.tmp
 
 done
+
+sort -k1,1 -k2,2n $sim_out.tmp > $sim_out
+rm $sim_out.tmp
 
 n_spm=$(awk 'END{print NR}' $spm_out)
 n_sim=$(awk 'END{print NR}' $spm_out)
